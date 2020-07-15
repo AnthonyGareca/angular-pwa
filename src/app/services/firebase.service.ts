3a82f4c3661a4d99000f2e5bcfa5f921edcc3d26
Anthony Gareca <anthonygarecaarias@gmail.com>
2020-07-14 23:34:35 -0400

    Add CRUD methods to firebaseService

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -8,4 +8,24 @@ import { Observable } from 'rxjs';
 export class FirebaseService {
     constructor(private firestore: AngularFirestore) {
   }
+
+  public create(resource: string, data: Object) {
+    return this.firestore.collection(resource).add(data);
+  }
+
+  public getById(resource: string, id: string) {
+    return this.firestore.collection(resource).doc(id).snapshotChanges();
+  }
+
+  public getAll(resource: string) {
+    return this.firestore.collection(resource).snapshotChanges();
+  }
+
+  public update(resource: string, id: string, data: Object) {
+    return this.firestore.collection(resource).doc(id).set(data);
+  }
+
+  public delete(resource: string, id: string) {
+    return this.firestore.collection(resource).doc(id).delete();
+  }
 }
